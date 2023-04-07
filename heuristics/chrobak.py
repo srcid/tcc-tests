ae13723a2342f4d08b06730d6377bde4e2abcc4f
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2023-04-07 20:59:57 -0300

    feat(heuristics): add chrobak (2004) heuristic function

---
+++ heuristics/chrobak.py
@@ -0,0 +1,18 @@
+from helpers.longest_common_substring_without_overlaping import (
+    longest_common_substring_without_overlaping,
+)
+
+
+def chrobak(S1: str, S2: str):
+    """
+    Returns a feasible solution for MSCP problem instance
+    """
+    blocks: dict[str, list[tuple[int, int]]] = {}
+
+    while sum(map(len,blocks.keys())) < len(S1):
+        s, pos_in_S1, pos_in_S2 = longest_common_substring_without_overlaping(S1, S2, blocks)
+        if s not in blocks.keys():
+            blocks[s] = []
+        blocks[s].append((pos_in_S1, pos_in_S2))
+
+    return blocks
