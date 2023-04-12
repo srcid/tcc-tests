e8df3e29acaa5aaa9beb7b2bb0bf4d4378300b67
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2023-04-12 15:10:36 -0300

    refact(chrobak): use an acc varible to control the loop

--- heuristics/chrobak.py
+++ heuristics/chrobak.py
@@ -8,11 +8,12 @@ def chrobak(S1: str, S2: str):
     Returns a feasible solution for MSCP problem instance
     """
     blocks: dict[str, list[tuple[int, int]]] = {}
-
-    while sum(map(len,blocks.keys())) < len(S1):
+    acc = 0
+    while acc < len(S1):
         s, pos_in_S1, pos_in_S2 = longest_common_substring_without_overlapping(S1, S2, blocks)
         if s not in blocks.keys():
             blocks[s] = []
         blocks[s].append((pos_in_S1, pos_in_S2))
+        acc += len(s)
 
     return blocks
