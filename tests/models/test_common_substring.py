d0cc2c49a62fa420f6a676511438fb93dd26f431
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2023-04-14 23:45:53 -0300

    refact(test common substring): correcting misswriting on test function name

--- tests/models/test_common_substring.py
+++ tests/models/test_common_substring.py
@@ -7,7 +7,7 @@ from models.common_substring import CommonSubstring
 @mark.parametrize(
         'S1,S2,optval', instances_g1
 )
-def test_common_blocks(S1, S2, optval):
+def test_common_substring(S1, S2, optval):
     m = CommonSubstring(S1, S2)
     r = m.solve('CPLEX', 1800000)
     val = r[0]
