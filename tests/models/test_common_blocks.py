40880ba69c43ca9f2a14728e25e21e84904ae497
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2023-04-15 12:57:28 -0300

    test(models): testing chrobak heuristic on both models

--- tests/models/test_common_blocks.py
+++ tests/models/test_common_blocks.py
@@ -1,6 +1,7 @@
 from instances import instances_g1
 from pytest import mark
 
+from heuristics.chrobak import chrobak
 from models.common_blocks import CommonBlocks
 
 
@@ -12,3 +13,13 @@ def test_common_blocks(S1, S2, optval):
     r = m.solve('CPLEX', 1800000)
     val = r[0]
     assert val == optval
+
+@mark.parametrize(
+        'S1,S2,optval', instances_g1
+)
+def test_common_blocks_with_chrobak_heuristic(S1, S2, optval):
+    m = CommonBlocks(S1, S2)
+    r = m.solve('CPLEX', 1800000, heuristic=chrobak)
+    val = r[0]
+    assert val == optval
+
--- tests/models/test_common_substring.py
+++ tests/models/test_common_substring.py
@@ -1,6 +1,7 @@
 from instances import instances_g1
 from pytest import mark
 
+from heuristics.chrobak import chrobak
 from models.common_substring import CommonSubstring
 
 
@@ -10,5 +11,14 @@ from models.common_substring import CommonSubstring
 def test_common_substring(S1, S2, optval):
     m = CommonSubstring(S1, S2)
     r = m.solve('CPLEX', 1800000)
+    val = r[0]
+    assert val == optval
+
+@mark.parametrize(
+        'S1,S2,optval', instances_g1
+)
+def test_common_substring_with_chrobak_heuristic(S1, S2, optval):
+    m = CommonSubstring(S1, S2)
+    r = m.solve('CPLEX', 1800000, heuristic=chrobak)
     val = r[0]
     assert val == optval
